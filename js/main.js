8344b6e164c3cfe927f81443c6237d5d3988ac6f
Luciano Riente <lucianotomasriente@gmail.com>
2021-04-19 02:38:37 -0300

    Se agregó el header responsive y el contenido principal no responsive

---
+++ js/main.js
@@ -0,0 +1,54 @@
+// Forma de simplificar los selectores para no escribirlos cada vez que se necesiten
+function $(selector) {
+	return document.querySelector(selector);
+}
+function $$(selector) {
+	return document.querySelectorAll(selector);
+}
+
+// ### VARIABLES ### //
+const header = $('.header');
+const menuButton = $('.menu-button');
+const lineMenuButton1 = $('.line-1');
+const lineMenuButton2 = $('.line-2');
+const lineMenuButton3 = $('.line-3');
+const ulLinksList = $('.ul-links-list');
+
+// ### FUNCIONES ### //
+function despliegueMenu() {
+	ulLinksList.classList.toggle('active');
+	lineMenuButton1.classList.toggle('active');
+	lineMenuButton2.classList.toggle('active');
+	lineMenuButton3.classList.toggle('active');
+}
+
+menuButton.addEventListener('click', () => {
+	despliegueMenu();
+});
+
+window.addEventListener('click', (e) => {
+	if (
+		ulLinksList.classList.contains('active') &&
+		e.target !== header &&
+		e.target !== menuButton &&
+		e.target !== lineMenuButton1 &&
+		e.target !== lineMenuButton2 &&
+		e.target !== lineMenuButton3
+	) {
+		despliegueMenu();
+	}
+});
+
+// Para que el header cambie cuando se scrollea //
+// Ubicación principal de la página (cuando está arriba de todo)
+let principalUbication = window.pageYOffset;
+window.onscroll = function () {
+	// Cuando se empieza a scrollear se toma el valor del scroll
+	let actualUbication = window.pageYOffset;
+	// Si se scrollea un poco se le aplica la sombra al header
+	if (principalUbication < actualUbication) {
+		$('.header').style.boxShadow = '2px 7px 7px -2px var(--primary-color)'; // #868686
+	} else {
+		$('.header').style.boxShadow = 'none';
+	}
+};
